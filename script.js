f54d37f8644b88547c24d0b75b150de257a83ad6
rankshow <uwaomaobinna20@gmail.com>
2022-09-09 12:32:22 -0500

    added rendering input workout

--- script.js
+++ script.js
@@ -11,12 +11,60 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 if(navigator.geolocation)
 navigator.geolocation.getCurrentPosition(function(position){
     const {latitude} = position.coords;
     const {longitude} = position.coords;
      console.log(`https://www.google.ng/maps/${latitude},${longitude}`);    
+   
+    // creating our own latitude and longitude
+    const coords = [latitude, longitude]
 
+    map = L.map('map').setView(coords, 13);
+
+L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Handling clicks on map
+map.on('click', function(mapE){
+    mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+
+})
 }, function() {
     alert('could not get your position');
 });
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    // clear input fields
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+
+    // Display the marker
+    console.log(mapEvent);
+    const {lat, lng} = mapEvent.latlng;
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnclick: false,
+            className: 'running-popup',
+        })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
+})
+
+inputType.addEventListener('change', function(){
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+})
+
+
